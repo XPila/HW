4808b3d1a5d55a57b83bfa3662f6069d28e3af0a
XPila <robert.pelnar@seznam.cz>
2015-11-28 01:00:27 +0100

    Update XHub.c

--- XPort/XHub.c
+++ XPort/XHub.c
@@ -208,18 +208,3 @@ int XHubPort_Tx(SXHubPort* pPort, void* pData, int iSize)
 	}
 	return iTx;
 }
-
-short hex(char c)
-{
-	static char* tab = "0123456789abcdef";
-	return tab[c>>4] | (tab[c&15] << 8);
-}
-
-char* hexstr(char* p, int n)
-{
-	short* str = (short*)malloc(2*n+1);
-	int i; for (i = 0; i < n; i++)
-		str[i] = hex(p[i]);
-	*(char*)(str+n) = 0;
-	return (char*)str;
-}
